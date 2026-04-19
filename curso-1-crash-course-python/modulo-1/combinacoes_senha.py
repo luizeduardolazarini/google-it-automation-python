a56572853226bbbacde6571f30334b73bb4a4d24
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-04-19 17:31:30 -0300

    feat: adiciona script de cálculo de combinações de senha (exponenciação)

---
+++ curso-1-crash-course-python/modulo-1/combinacoes_senha.py
@@ -0,0 +1,7 @@
+# Cálculo de possibilidades de senhas numéricas (0-9)
+digitos_possiveis = 10
+tamanho_senha = 3
+
+total_combinacoes = digitos_possiveis ** tamanho_senha
+
+print(total_combinacoes)
