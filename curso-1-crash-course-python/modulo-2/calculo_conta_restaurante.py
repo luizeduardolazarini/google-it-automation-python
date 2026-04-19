e001ccf880970ea121aa55f5d029d18b65b932e5
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-04-19 18:46:54 -0300

    feat: script para cálculo de divisão de conta com gorjeta

--- curso-1-crash-course-python/modulo-2/calculo_conta_restaurante.py
+++ curso-1-crash-course-python/modulo-2/calculo_conta_restaurante.py
@@ -2,4 +2,4 @@ bill: float = 47.28
 tip: float = bill * 0.15 
 total: float = bill + tip 
 share: float = total / 2 
-print(f"Each person needs to pay {share}") 
+print(f"Each person needs to pay {share:.2f}") 
