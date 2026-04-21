ac92f8b9c24c7e28b5f38f4bcacd58b12c5a80d0
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-04-21 19:24:56 -0300

    feat: aplicar cálculos matemáticos reutilizáveis com funções e f-strings

---
+++ curso-1-crash-course-python/modulo-2/funcoes_area.py
@@ -0,0 +1,9 @@
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+area_a = area_triangulo(10, 5)
+area_b = area_triangulo(7, 3)
+sum_area = area_a + area_b
+print(f"Área do triângulo A: {area_a}")
+print(f"Área do triângulo B: {area_b}")
+print(f"Soma das áreas: {sum_area}")
